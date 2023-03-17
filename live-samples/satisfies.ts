c01b67beb389fee2a5369319b89f614a140b612f
Thomas Klepzig <thomas.klepzig@ivx.com>
2023-03-17 11:38:43 +0100

    satisfies

--- live-samples/satisfies.ts
+++ live-samples/satisfies.ts
@@ -1,19 +1,56 @@
-//type Colors = "red" | "green" | "blue";
-//type RGB = [red: number, green: number, blue: number];
+type Color = "FancyBlue" | "DarkBlack" | "BrightWhite";
 
-//const palette = {
-//red: [255, 0, 0],
-//green: "#00ff00",
-//blue: [0, 0, 255],
-//} satisfies Record<Colors, string | RGB>;
+type HexLetter = "a" | "b" | "c" | "d" | "e" | "f";
+type HexNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type HexColorPart = HexLetter | HexNumber;
+type HexColor = `#${HexColorPart}${HexColorPart}${HexColorPart}`;
+type RGB = [number, number, number];
 
-//const redComponent = palette.red[0];
-//const greenNormalized = palette.green.toUpperCase();
+//const colors: Record<Color, HexColor | RGB> = {
+//FancyBlue: { value: "blue" },
+//DarkBlack: [0, 0, 0],
+//BrightWhite: "#fff",
+//};
 
-interface B {
-  age: number;
-}
+//colors.BrightWhite.toUpperCase();
 
-const b = JSON.stringify({ blubb: 42 });
-const c: B = JSON.parse(b) satisfies B;
-console.dir(c.age);
+//const colors = {
+//FancyBlue: "#00f",
+//DarkBlack: [0, 0, 0],
+//BrightWhite: "#fff",
+//} satisfies Record<Color, HexColor | RGB>;
+
+//colors.BrightWhite.toUpperCase();
+
+const crew: Record<string, unknown> = {
+  Alara: "open the jar of pickles",
+  Bortus: { hatchEgg: 21 },
+  Isaac: 0b0101,
+};
+
+const whatIsGordonDoing = crew.Gordon;
+const howLongHatching = crew.Bortus.hatchEgg;
+
+//type Activity =
+//| { species: "xelayan"; activity: string }
+//| { species: "moclan"; activity: { hatchEgg: number } }
+//| { species: "kaylon"; activity: number };
+//const crew: Record<string, Activity> = {
+//Alara: { species: "xelayan", activity: "open the jar of pickles" },
+//Bortus: { species: "moclan", activity: { hatchEgg: 21 } },
+//Isaac: { species: "kaylon", activity: 0b0101 },
+//};
+
+//const whatIsGordonDoing = crew.Gordon;
+//const howLongHatching =
+//crew.Bortus.species === "moclan" && crew.Bortus.activity.hatchEgg;
+
+//const crew = {
+//Alara: "open the jar of pickles",
+//Bortus: { hatchEgg: 21 },
+//Isaac: 0b0101,
+//} satisfies Record<string, unknown>;
+
+//const whatIsGordonDoing = crew.Gordon;
+//const whatIsAlaraDoing = crew.Alara;
+//const howLongHatching = crew.Bortus.hatchEgg;
